1f71691e01d3bd78842c9ca19f3c128bf2c6c05f
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-10-04 12:39:12 +0300

    create private pretty_print_json_data function in order to properly display json file in console

--- Python_files_manipulation/JSONManipulation.py
+++ Python_files_manipulation/JSONManipulation.py
@@ -2,29 +2,41 @@ import simplejson as json
 from pathlib import Path
 
 
+global_folder_path = Path('C:\Learning_Python_Scripts\Python_files_manipulation')
+get_all_countries_file = 'GetAllCountries.json'
+quiz_json_file = 'quiz.json'
+
+
 class JSONManipulation:
 
-    folder_path = Path('C:\Learning_Python_Scripts\Python_files_manipulation')
-    file_to_open = folder_path / 'GetAllCountries.json'
+    def __init__(self, json_file):
+        self.input_file = global_folder_path / json_file
 
     def read_json_file(self):
-        if self.file_to_open.exists():
-            print("Read JSON from path: {}".format(self.file_to_open))
-            with open(self.file_to_open.name, 'r', encoding="utf-8") as input_json:
+        if self.input_file.exists():
+            print("Read JSON from path: {}".format(self.input_file))
+            with open(self.input_file.name, 'r', encoding="utf-8") as input_json:
                 inp_data = input_json.read()
+                self._pretty_print_json_data(inp_data)
                 return inp_data
         else:
-            print("File not found on path: {}".format(self.file_to_open))
+            print("File not found on path: {}".format(self.input_file))
+
+    @staticmethod
+    def _pretty_print_json_data(input_file, sort=True, indents=4):
+        if type(input_file) is str:
+            print(json.dumps(json.loads(input_file), sort_keys=sort, indent=indents))
+        else:
+            print(json.dumps(input_file, sort_keys=sort, indent=indents))
+        return None
 
 
 if __name__ == "__main__":
-    jsonM_obj = JSONManipulation()
-    j_data = jsonM_obj.read_json_file()
-    print(type(j_data))
-    # utf-8-sig
-    print(json.loads(j_data))
-    print(type(json.loads(j_data)))
-    print(jsonM_obj.file_to_open)
+    print('\nDisplay {} file '.format(get_all_countries_file))
+    countries_json_obj = JSONManipulation(get_all_countries_file).read_json_file()
+    print('\nDisplay {} file '.format(quiz_json_file))
+    quiz_json_obj = JSONManipulation(quiz_json_file).read_json_file()
+
 
 
 
