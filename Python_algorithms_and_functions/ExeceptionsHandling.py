7238b045839d12a2758f2773af5dded090c281f5
Luiza <luiza.mihaiuc@gmail.com>
2018-11-15 10:48:10 +0200

    modify ExceptionHandling methods for unreached code

--- Python_algorithms_and_functions/ExeceptionsHandling.py
+++ Python_algorithms_and_functions/ExeceptionsHandling.py
@@ -1,4 +1,4 @@
-"""Unhandled exections will terminate the program """
+"""Unhandled exceptions will terminate the program """
 import sys
 from math import log
 
@@ -6,8 +6,8 @@ from math import log
 def convert_to_int(s):
     """Handle multiple exception in a single try except block"""
     try:
-        return int(s)
         print('Conversion succeeded! for item {}'.format(s))
+        return int(s)
     # exception accepts tuple of errors type
     except (ValueError, TypeError) as exp:
         print("Conversion failed for item {}: {}"
@@ -51,13 +51,13 @@ if __name__ == "__main__":
     divide_two_numbers('star', 3)
     divide_two_numbers('star', 0)
     divide_two_numbers(0, 0)
-    sys.exit()
     try:
         print(square_root(9))
         print(square_root(2))
         print(square_root(-1))
     except ValueError as e:
         print(e, file=sys.stderr)
+        sys.exit()
 
     number_type = convert_to_int("2")
     print(type(number_type))
