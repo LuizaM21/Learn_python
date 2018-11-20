6b99e945fa20b22836a6a1ec3b3859d21ebd1d07
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-11-20 13:22:47 +0200

    modify a list using while loop

--- Python_program_functions/Program_functions.py
+++ Python_program_functions/Program_functions.py
@@ -1,15 +1,39 @@
-"""Interrupt a program only when the specific key is being used"""
+
+
+def decrease_list_of_users():
+    """ iterate over a list using while loop
+        If we modify a list using a for loop,
+        python will loose track of the items in the list.
+        For all types of modifications we can use while loop"""
+
+    unconfirmed_users = ['tony', 'frances', 'mike', 'katie', 'jack']
+    confirmed_users = []
+
+    while unconfirmed_users:
+        print("\nLength of unconfirmed_users: ", len(unconfirmed_users))
+
+        current_user = unconfirmed_users.pop()
+        print("current user: ", current_user.title())
+
+        confirmed_users.append(current_user)
+        print("length of confirmed_users: ", len(confirmed_users))
 
 
 def main():
+    """Interrupt a program only when the specific key is being used"""
+
     prompt = "\nTo end this process press 'q'."
     prompt += "\nPlease enter your name: "
 
-    message = ""
-
-    while message != 'q':
+    active = True
+    while active:
         message = input(prompt)
-        print(message)
+        if message == 'q':
+            active = False
+        else:
+            print(message)
+
+    decrease_list_of_users()
 
 
 if __name__ == '__main__':
