721b82f4b5001d8e326a6f899b53e871df5b4525
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-11-21 17:19:30 +0200

    update dict method to create dictiony in dictionary

--- Python_program_functions/Dictionary_update_with_user.py
+++ Python_program_functions/Dictionary_update_with_user.py
@@ -2,7 +2,7 @@ import pprint
 
 
 def update_a_dictionary():
-    country_details = {}
+    countries_details = {}
     country = True
 
     while country:
@@ -10,10 +10,17 @@ def update_a_dictionary():
 
         country_name = input("Insert country name: ")
         country_capital = input("Enter country capital: ")
-        country_details.setdefault(country_name.title(), country_capital.title())
-        pprint.pprint(country_details)
+        country_currency = input("Enter country currency: ")
+        country_ISO2 = input("Enter ISO2 code for this country: ")
+
+        country_details = countries_details.setdefault(country_name.title(), {})
+        country_details.setdefault("Capital city", country_capital.title())
+        country_details.setdefault("Currency", country_currency.upper())
+        country_details.setdefault("ISO2_code", country_ISO2.upper())
+
         # flag necessary to break the while loop if user hits q key
-        repeat = input("Insert a country and a capital. To finish the process press 'q'")
+        repeat = input("In order to insert another country details press enter. \nTo finish the process press 'q'\n")
+        pprint.pprint(countries_details)
         if repeat != 'q':
             continue
         else:
