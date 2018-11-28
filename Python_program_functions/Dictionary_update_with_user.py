ec122c98a6de1a13df5dfa7d4941fa2afdb386ee
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-11-28 15:53:01 +0200

    create class to update a dictionary of countries

--- Python_program_functions/Dictionary_update_with_user.py
+++ Python_program_functions/Dictionary_update_with_user.py
@@ -8,15 +8,18 @@ def update_a_dictionary():
     while country:
         """Update an empty dictionary with user input """
 
-        country_name = input("Insert country name: ")
-        country_capital = input("Enter country capital: ")
-        country_currency = input("Enter country currency: ")
-        country_ISO2 = input("Enter ISO2 code for this country: ")
+        country_name = input("Insert country name:\n")
+        country_capital = input("Enter country capital:\n")
+        country_currency = input("Enter country currency:\n")
+        country_ISO2 = input("Enter ISO2 code for this country:\n")
+        country_cities = input("Enter a list of cities for this country:\n")
 
+        # create a subdivision dictionary to group the details for each inserted country
         country_details = countries_details.setdefault(country_name.title(), {})
         country_details.setdefault("Capital city", country_capital.title())
         country_details.setdefault("Currency", country_currency.upper())
         country_details.setdefault("ISO2_code", country_ISO2.upper())
+        country_details.setdefault("country cities", [country_cities])
 
         # flag necessary to break the while loop if user hits q key
         repeat = input("In order to insert another country details press enter. \nTo finish the process press 'q'\n")
