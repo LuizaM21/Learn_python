378d369daf000621308a5db3eea56dbed44dcd8f
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-11-29 17:34:59 +0200

    create nu package with classes with inheritance flow

---
+++ Python_OOP_inheritance/Countries.py
@@ -0,0 +1,25 @@
+
+
+class Countries:
+
+    def __init__(self, capital_city, residents_number, mountains=False, sea=False):
+        self.capital = capital_city
+        self.resident_num = residents_number
+        self.has_mountains = mountains
+        self.has_sea = sea
+
+    def set_capital_city(self, capital_country):
+        """Set the city capital of a country"""
+        self.capital = capital_country
+
+    def get_capital_city(self):
+        """Return the city capital of a country"""
+        return self.capital.title()
+
+    def set_country_resident_number(self, residents_num):
+        """Set the total number of residents of a country"""
+        self.resident_num = residents_num
+
+    def set_country_demographics(self, has_mountains, has_sea):
+        self.has_mountains = has_mountains
+        self.has_sea = has_sea
