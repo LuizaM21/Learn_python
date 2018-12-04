a1a70c34b2d15bf4faafaf69a2571b4900205acb
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-12-04 16:09:46 +0200

    create instance of a class as atribute

--- Python_OOP_inheritance/Countries.py
+++ Python_OOP_inheritance/Countries.py
@@ -33,23 +33,39 @@ class Countries(object):
         return country_details
 
 
+class CityDetails:
+    def __init__(self, city_name, city_location, capital=False):
+        self.city_name = city_name
+        self.city_location = city_location
+        self.is_capital = capital
+
+    def get_city_details(self):
+        city_details = "Situated in {0} of the country \nCity capital of the country = {1}"\
+                        .format(self.city_location, str(self.is_capital))
+        return city_details
+
+
 class EnglandCountry(Countries):
     """Represent all the particularities that this country has"""
 
-    def __init__(self, capital_city, residents_number, mountains=False, sea=False, ocean=True, islands_num=3):
+    def __init__(self, capital_city, residents_number, mountains=False, sea=False, ocean=False, islands_num=3):
         """initialize attributes for England country"""
         self.has_ocean = ocean
         self.number_of_isles = islands_num
+
         # bounds the parent class init method with the inherited class init method
         super().__init__(capital_city, residents_number, mountains, sea)
+        self.city_details = CityDetails(capital_city, "North-East", True)
 
     def get_england_extra_details(self):
         england_details = "Country has ocean opening: {0}\n" \
-                          "Country number of isles: {1}".format(self.has_ocean, self.number_of_isles)
+                          "Country number of isles: {1}".format(str(self.has_ocean), str(self.number_of_isles))
         return england_details
 
 
 if __name__ == '__main__':
-    england_country = EnglandCountry('London', 100000, False, True, False, 4)
+    england_country = EnglandCountry('London', 100000, False, True, True, 4)
     print(england_country.get_all_country_details())
     print(england_country.get_england_extra_details())
+    print(england_country.city_details.get_city_details())
+
