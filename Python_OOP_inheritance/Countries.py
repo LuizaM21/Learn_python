2dc2d0f3a5178f899138f4ffb82eea4fdc192da1
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2018-12-04 14:32:31 +0200

    create get_england_extra_details() specific for child class

--- Python_OOP_inheritance/Countries.py
+++ Python_OOP_inheritance/Countries.py
@@ -1,6 +1,5 @@
 
-
-class Countries:
+class Countries(object):
 
     def __init__(self, capital_city, residents_number, mountains=False, sea=False):
         self.capital = capital_city
@@ -25,9 +24,32 @@ class Countries:
         self.has_mountains = has_mountains
         self.has_sea = has_sea
 
-class Country(Countries):
-    """Represent all the particularities that a class has"""
+    def get_all_country_details(self):
+        """Return all the common details of a country"""
+        country_details = "Country capital: {0} " \
+                          "\nCountry residence number: {1} " \
+                          "\nCountry has mountains: {2} " \
+                          "\nCountry has sea: {3}".format(self.capital, str(self.resident_num), str(self.has_mountains), str(self.has_sea))
+        return country_details
+
+
+class EnglandCountry(Countries):
+    """Represent all the particularities that this country has"""
+
+    def __init__(self, capital_city, residents_number, mountains=False, sea=False, ocean=True, islands_num=3):
+        """initialize attributes for England country"""
+        self.has_ocean = ocean
+        self.number_of_isles = islands_num
+        # bounds the parent class init method with the inherited class init method
+        super().__init__(capital_city, residents_number, mountains, sea)
+
+    def get_england_extra_details(self):
+        england_details = "Country has ocean opening: {0}\n" \
+                          "Country number of isles: {1}".format(self.has_ocean, self.number_of_isles)
+        return england_details
 
 
 if __name__ == '__main__':
-    pass
+    england_country = EnglandCountry('London', 100000, False, True, False, 4)
+    print(england_country.get_all_country_details())
+    print(england_country.get_england_extra_details())
