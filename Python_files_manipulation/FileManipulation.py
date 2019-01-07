ca8553bce17817907dc2fcdd4f2785042097860c
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2019-01-07 17:24:58 +0200

    create FileNotFoundEror handling for FileHandling class

--- Python_files_manipulation/FileManipulation.py
+++ Python_files_manipulation/FileManipulation.py
@@ -18,46 +18,75 @@ class FileManipulation:
     def __init__(self, input_file):
         self.file = input_file
 
-    # automatically close the file using context manager
     def read_file(self):
-        with open('{}'.format(self.file), 'r') as f:
-            f_content = f.read()
+        """Automatically close the file using context manager"""
+        try:
+            with open('{}'.format(self.file), 'r') as f:
+                f_content = f.read()
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
             return f_content
 
-    # create a file and write into it
     def write_file(self):
-        print('Call write_file function from FileManipulation class')
-        with open('{}'.format(self.file), 'w') as f:
-            print('create and write into file {} '.format(f.name))
-            print('Apply mode: {}'.format(f.mode))
-            file_lines = ['name: Zimbabwe\n', 'alpha2_code: ZW\n', 'alpha3_code: ZWE\n']
-            f.writelines(file_lines)
-        return True
+        """Create a file and write into it"""
+        try:
+            with open('{}'.format(self.file), 'w') as f:
+                print('create and write into file {} '.format(f.name))
+                print('Apply mode: {}'.format(f.mode))
+                file_lines = ['name: Zimbabwe\n', 'alpha2_code: ZW\n', 'alpha3_code: ZWE\n']
+                f.writelines(file_lines)
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
+            return True
 
-    # create a copy of an existent file
     def copy_text_file(self, output_file):
-        print('\nCall copy_text_file function from FileManipulation class')
-        with open('{}'.format(self.file), 'r') as read_f:
-            with open('{}'.format(output_file), 'w') as write_f:
-                for line in read_f:
-                    write_f.write(line)
-        return True
+        """Create a copy of an existent file"""
+        try:
+            with open('{}'.format(self.file), 'r') as read_f:
+                with open('{}'.format(output_file), 'w') as write_f:
+                    for line in read_f:
+                        write_f.write(line)
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
+            return True
 
-    # copy file in binary mode
     def copy_binary_file(self, output_file):
-        print('\nCall copy_binary_file function from FileManipulation class')
-        with open('{}'.format(self.file), 'rb') as read_b:
-            with open('{}'.format(output_file), 'wb') as copy_b:
-                for line in read_b:
-                    copy_b.write(line)
-        return True
+        """Copy file in binary mode"""
+        try:
+            with open('{}'.format(self.file), 'rb') as read_b:
+                with open('{}'.format(output_file), 'wb') as copy_b:
+                    for line in read_b:
+                        copy_b.write(line)
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
+            return True
 
     def add_to_existent_file(self):
-        print('\nCall add_to_existent_file function from FileManipulation class\n')
-        with open('{}'.format(self.file), 'a+') as append_f:
-            new_lines = ['name: Afghanistan\n', 'alpha2_code: AF\n', 'alpha3_code: AFG\n']
-            append_f.writelines(new_lines)
-            append_f.read()
-        return True
+        """Add content to existent csv file"""
+        try:
+            with open('{}'.format(self.file), 'a+') as append_f:
+                new_lines = ['name: Afghanistan\n', 'alpha2_code: AF\n', 'alpha3_code: AFG\n']
+                append_f.writelines(new_lines)
+                append_f.read()
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
+            return True
+
+    def count_words(self):
+        """Count the number of word in a file"""
+        try:
+            with open('{}'.format(self.file)) as f:
+                content = f.read()
+        except FileNotFoundError:
+            print("No such file or directory! ", self.file)
+        else:
+            words = content.split()
+            number_words = len(words)
+            return number_words
 
 
