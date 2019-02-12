59e0c5ad6879ffccac50ff949a5b8509905b326d
Luiza Mihaiuc <luiza.mihaiuc@enea.com>
2019-02-12 15:20:24 +0200

    WIP create parallel process for genios site

---
+++ Python_server_testing/Genios_threads.py
@@ -0,0 +1,50 @@
+from timeit import default_timer as timer
+import os
+
+import bs4
+import urllib.request
+from multiprocessing import Process
+
+
+URL_lists = ['https://www.genios.ro/catalog/moyu/',
+             'https://www.genios.ro/catalog/lanlan/',
+             'https://www.genios.ro/catalog/philos/',
+             'https://www.genios.ro/catalog/mefferts/',
+             'https://www.genios.ro/catalog/mofangge/',
+             'https://www.genios.ro/catalog/shengshou/',
+             'https://www.genios.ro/catalog/yj/',
+             'https://www.genios.ro/catalog/yuxin/',
+             'https://www.genios.ro/catalog/mf8/',
+             'https://www.genios.ro/catalog/calvin/',
+             'https://www.genios.ro/catalog/qiyi/']
+
+
+def get_page_content(site_url):
+    """Loads html content of the site"""
+    process_id = os.getpid()
+    print("process_id:", process_id)
+    with urllib.request.urlopen(site_url) as response:
+        if 'text/html' in response.headers['Content-Type']:
+            current_header = bs4.BeautifulSoup(response.read(), features="html.parser").find(id="header")
+            return current_header
+        raise ValueError('Not a valid HTML page.')
+
+
+if __name__ == '__main__':
+
+    process_list = []
+    for site in URL_lists:
+        start_time = timer()
+        process = Process(target=get_page_content, args=(site,))
+        process_list.append(process)
+
+        process.start()
+        end_time = timer()
+        duration = end_time - start_time
+        print("\ncalled link: {2}\nprocess name: {0}\nduration: {1:.2f} seconds\n".format(process.name, duration, site))
+
+    for proc in process_list:
+        proc.join()
+
+
+
